fd771823bf40fa8d1b7f3822bbc95a09c5130155
jindac <dachanman91@gmail.com>
2016-12-13 20:10:04 -0800

    Switched from hrsf53 repo

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var found = [];
+  var findNodes = function (value) {
+    var currentNode = value.childNodes;
+    var splitClass;
+    for (var i = 0; i < currentNode.length; i++) {
+      if (currentNode[i].className !== undefined) {
+        splitClass = currentNode[i].className.split(' ');
+        //console.log("splitClass = ",splitClass);
+        for (var j = 0; j < splitClass.length; j++) {
+          if (splitClass[j] === className) {
+            found.push(currentNode[i]); 
+            //console.log("found = ", found);
+          }
+        }
+      }
+      if (currentNode[i].childNodes.length !== 0) {
+        findNodes(currentNode[i]);
+      }
+    }    
+  };
+  if (document.body.className === className) {
+    found.push(document.body);
+  }
+  findNodes(document.body);
+  return found;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+  var stringify = function(value) {
+    //if statement to check if array or string 
+    if (Array.isArray(value)) {
+      result += '[';
+      _.each(value, function(value, index, collection) {
+        stringify(value);
+        if (index !== collection.length - 1) {
+          result += ',';
+        }
+      });
+      result += ']';
+    } else if (typeof value === 'object' && value !== null) {
+      result += '{';
+      // console.log('value =', value);
+      if (!jQuery.isEmptyObject(value)) {
+        _.each(value, function(value, key, collection) {
+          if (typeof value !== 'function' && value !== undefined) {
+            // console.log('in each');
+            result += '\"' + key + '\"' + ':';
+            stringify(value);
+            result += ',';
+          }
+        });
+        // console.log('result: ', result);
+        if (!(result.charAt(result.length - 1) === '{')) {
+          result = result.substring(0, result.length - 1);
+        }
+      }
+      // console.log('result = ', result);
+      result += '}';
+    } else if (typeof value === 'number' || typeof value === 'boolean') {
+      result += value.toString();
+      //stringify();
+    } else if (value === null) {
+      result += 'null';
+    } else if (typeof value === 'string') {
+      result += '\"' + value + '\"';
+    }
+  };
+  stringify(obj);
+  return result;
 };
